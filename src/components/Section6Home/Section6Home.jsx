a12184ea59b4c02caef05e2d0b24010b62dafa7b
shafin90 <mashrafiahnam1@gmail.com>
2024-03-10 06:05:45 -0700

    section6 of home is complete

---
+++ src/components/Section6Home/Section6Home.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const Section6Home = () => {
+    return (
+        <div className=' flex flex-col md:flex-row justify-between items-start mx-auto w-[94vw]'>
+
+            {/* Left side text ===================== */}
+            <div className=' w-full md:w-4/12 pt-3 mb-5 md:mb-0'>
+
+                <h1 className=' font-bold text-5xl text-black mb-6 mt-6 leading-none'>
+                    Edit generated images with AI
+                </h1>
+
+                <article className=' text-xl leading-9 mb-5'>
+                    Pebblely isn't just an AI image generator but more like a design tool.
+                </article>
+
+                <a className=" mb-3 hover:translate-x-3 transition-all btn text-base bg-amber-300 hover:bg-amber-400 font-bold text-gray-900 px-6">Get 40 Add multiple products
+                </a>
+
+                <a className="mb-3 hover:translate-x-3 transition-all btn text-base bg-amber-300 hover:bg-amber-400 font-bold text-gray-900 px-6">Get 40 Edit generated images
+                </a>
+
+                <a className="hover:translate-x-3 transition-all btn text-base bg-amber-300 hover:bg-amber-400 font-bold text-gray-900 px-6">Get 40 Reuse a background
+                </a>
+            </div>
+
+            {/* Right side image  container*/}
+            <div className=' w-full md:w-8/12'>
+                <img
+                    className=' w-full'
+                    src="/assets/images/section6.jpg" alt="" />
+            </div>
+
+        </div>
+    );
+};
+
+export default Section6Home;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,17 +6,19 @@ import VideoSection from '../../components/VideoSection/VideoSection';
 import Section3Home from '../../components/Section3Home/Section3Home';
 import Section4Home from '../../components/Section4Home/Section4Home';
 import Section5Home from '../../components/Section5Home/Section5Home';
+import Section6Home from '../../components/Section6Home/Section6Home';
 
 const Home = () => {
     return (
         <div className=' w-full'>
-            <Navbar/>
-            <HeroSection/>
-            <Companies/>
-            <VideoSection/>
-            <Section3Home/>
-            <Section4Home/>
-            <Section5Home/>
+            <Navbar />
+            <HeroSection />
+            <Companies />
+            <VideoSection />
+            <Section3Home />
+            <Section4Home />
+            <Section5Home />
+            <Section6Home />
         </div>
     );
 };
